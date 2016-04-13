d539b35bf174329b00a7f842897dd13ec5ab53ee
annalizhaz <annalizhaz@gmail.com>
2016-04-12 23:13:15 -0500

    Fully functioning pipeline

--- hw2/pipeline.py
+++ hw2/pipeline.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import random
+from sklearn.linear_model import LogisticRegression
 
 def get_description(df, file_name):
     df1 = df.describe().T
@@ -59,16 +61,25 @@ def cat_to_binary(df, column_headers):
 
     return new_df
 
-def build(df):
-    return
+def build_train_test(df_train, df_test, predictor_columns, predicted_column):
+    model = LogisticRegression()
+    model.fit(df_train[predictor_columns], df_train[predicted_column])
+    predicted_results = model.predict(df_test[predictor_columns])
+    actual_results = df_test[predicted_column]
+    actual_results.rename(index="true_results", inplace=True)
+    predicted_results_se = pd.Series(predicted_results, name="test_results")
+    true_df = pd.DataFrame(actual_results)
+    true_df.reset_index(drop=True, inplace=True)
+    test_df = pd.DataFrame(predicted_results_se)
+    comparison = true_df.join(test_df, how="left")
+    comparison.loc[comparison["true_results"]==comparison["test_results"], "match"] = 1
+    print(true_df.head())
+    print(test_df.head())
+    print(len(df_test))
+    print(len(df_train))
+    return (comparison["match"].sum()/len(df_test))*100
 
-def predict(df):
-    return
-
-def test_accuracy(results, truth):
-    return
-
-def hw2_go(file_name, desc_file_name, has_index=True):
+def hw2_go(file_name, desc_file_name, predict, drop_attr, has_index=True):
     '''
     function to call other functions as needed for hw2
     hard coded variables here
@@ -83,5 +94,20 @@ def hw2_go(file_name, desc_file_name, has_index=True):
     df1 = fill_in(df)
     df2 = discretize_q(df1, ["age"], 4)
     df3 = cat_to_binary(df2, ["age_cat"])
-    #build(df3)
-    #predict(df4)
+
+    predictor_columns = list(set(df3.columns) - set(drop_attr))
+
+    rand_list = random.sample(range(len(df3)), len(df3))
+    divide_point = int(.8*len(df3))
+
+    train_list = rand_list[:divide_point]
+    test_list = rand_list[divide_point:]
+
+    train_df = df3.iloc[train_list, :]
+    test_df = df3.iloc[test_list, :]
+
+    accuracy = build_train_test(train_df, test_df, predictor_columns, predict)
+
+    return accuracy
+
+#hw2_go("cs-training.csv", "test5.csv", "SeriousDlqin2yrs", ["SeriousDlqin2yrs", "NumberOfTimes90DaysLate", "RevolvingUtilizationOfUnsecuredLines", "NumberOfTime30-59DaysPastDueNotWorse", "NumberOfTime30-59DaysPastDueNotWorse", "NumberOfTime30-59DaysPastDueNotWorse", "NumberOfTime30-59DaysPastDueNotWorse", "DebtRatio", "NumberOfDependents", "MonthlyIncome"])
